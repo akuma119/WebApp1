f3fc43847fdfb4bde9ef3806de7275ab7ddd6c9d
Néstor Gustavo Salvi <nsalvi.itsynch@outlook.com.ar>
2019-05-10 15:40:27 -0300

    trying to make a web app

--- WebApp1/wwwroot/js/site.js
+++ WebApp1/wwwroot/js/site.js
@@ -2,3 +2,50 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function Love() {
+    result = document.getElementById("resultlove");
+    if (Math.random() >= 0.5) {
+        result.innerHTML = "Yes! ❤️ :D"
+    }
+    else {
+        result.innerHTML = "No :("
+    }
+}
+
+function UnicornName() {
+    result = document.getElementById("resultname");
+    firstNameList = [
+        'Flashy',
+        'Sleepy',
+        'Graceful',
+        'Shiny',
+        'Woopsy',
+        'Tangled',
+        'Wonder',
+        'White',
+        'Pink',
+        'Pastel',
+        'Fluffy',
+        'Friendly',
+        'Lovely',
+        'Magic'
+    ];
+    lastNameList = [
+        'Snow',
+        'Wings',
+        'Hooves',
+        'Mane',
+        'Fantasy',
+        'Dream',
+        'Light',
+        'Hug',
+        'Breeze',
+        'Heart',
+        'Smile',
+        'Magic'
+    ]
+    result.innerHTML =
+        firstNameList[Math.floor(Math.random() * firstNameList.length)]
+        + " " +
+        lastNameList[Math.floor(Math.random() * lastNameList.length)];
+}
